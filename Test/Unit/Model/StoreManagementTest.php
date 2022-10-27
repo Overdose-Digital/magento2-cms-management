0f802ada8454f1aa67698839e2779ea682a9bc25
Omair Khalid <omair.khalid@overdose.digital>
2022-10-27 19:39:41 +0000

    Improved unit tests by adding more test cases and improving checking logic. https://www.wrike.com/open.htm?id=914540466

--- Test/Unit/Model/StoreManagementTest.php
+++ Test/Unit/Model/StoreManagementTest.php
@@ -42,58 +42,152 @@ class StoreManagementTest extends TestCase
     }
 
     /**
+     * @return array
+     */
+    public function dataProviderGetStoreIdsByCodes(): array
+    {
+        return [
+            'store_not_found_case' => [
+                'storeCodes' => ['us'],
+                'expectedResult' => []
+            ],
+            'store_found_case' => [
+                'storeCodes' => ['nz'],
+                'expectedResult' => [1]
+            ],
+            'admin_case' => [
+                'storeCodes' => ['admin'],
+                'expectedResult' => [0]
+            ],
+            'multi_store_case' => [
+                'storeCodes' => ['admin', 'nz', 'us', 'au'],
+                'expectedResult' => [0, 1, 2]
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderGetStoreIdsByCodes
      * @return void
      */
-    public function testGetStoreIdsByCodes()
+    public function testGetStoreIdsByCodes($storeCodes, $expectedResult)
     {
-        $storeCodes = ['admin', 'nz'];
-
-        $this->storeInterfaceMock->expects($this->atLeastOnce())
+        $this->storeInterfaceMock
             ->method('getId')
             ->willReturn(1);
 
-        $this->storeRepositoryMock->expects($this->atLeastOnce())
+        $auStore = $this->getMockBuilder(StoreInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $auStore->method('getId')
+            ->willReturn(2);
+
+        $this->storeRepositoryMock
             ->method('get')
-            ->with('nz')
-            ->willReturn($this->storeInterfaceMock);
+            ->withConsecutive(['nz'], ['au'])
+            ->willReturnOnConsecutiveCalls($this->storeInterfaceMock, $auStore);
 
         $this->storeRepositoryMock->expects($this->atLeastOnce())
             ->method('getList')
-            ->willReturn(['admin' => 'adminStore', 'nz' => 'nzStore']);
-
-        $expectedResult = [0, 1];
+            ->willReturn(['admin' => 'adminStore', 'nz' => 'nzStore', 'au' => 'auStore']);
 
         $this->assertEquals($expectedResult, $this->model->getStoreIdsByCodes($storeCodes));
     }
 
     /**
-     * @return void
+     * @return array
      */
-    public function testFilterStoresByStoreCodes()
+    public function dataProviderFilterStoresByStoreCodes(): array
     {
-        $storeCodes = ['admin', 'nz'];
-        $this->storeRepositoryMock->expects($this->atLeastOnce())
-            ->method('getList')
-            ->willReturn(['admin' => 'adminStore', 'nz' => 'nzStore']);
-
-        $this->assertSame($storeCodes, $this->model->filterStoresByStoreCodes($storeCodes));
+        return [
+            'store_not_found_case' => [
+                'storeCodes' => ['pk'],
+                'expectedResult' => []
+            ],
+            'store_found_case' => [
+                'storeCodes' => ['nz'],
+                'expectedResult' => ['nz']
+            ],
+            'admin_case' => [
+                'storeCodes' => ['admin'],
+                'expectedResult' => ['admin']
+            ],
+            'multi_store_case' => [
+                'storeCodes' => ['admin', 'au', 'nz', 'ua'],
+                'expectedResult' => ['admin', 'nz']
+            ]
+        ];
     }
 
     /**
+     * @dataProvider dataProviderFilterStoresByStoreCodes
      * @return void
      */
-    public function testFilterStoresByStoreIds()
+    public function testFilterStoresByStoreCodes($storeCodes, $expectedResult)
     {
-        $storeIds = [1, 2];
+        $this->storeRepositoryMock->expects($this->once())
+            ->method('getList')
+            ->willReturn(['admin' => 'adminStore', 'nz' => 'nzStore']);
+
+        $this->assertSame($expectedResult, $this->model->filterStoresByStoreCodes($storeCodes));
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderFilterStoresByStoreIds(): array
+    {
+        return [
+            'store_not_found_case' => [
+                'storeIds' => [77],
+                'expectedResult' => []
+            ],
+            'store_found_case' => [
+                'storeIds' => [1],
+                'expectedResult' => [1]
+            ],
+            'admin_case' => [
+                'storeIds' => [0],
+                'expectedResult' => [0]
+            ],
+            'multi_store_case' => [
+                'storeIds' => [0, 1, 2, 77],
+                'expectedResult' => [0, 1, 2]
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderFilterStoresByStoreIds
+     * @return void
+     */
+    public function testFilterStoresByStoreIds($storeIds, $expectedResult)
+    {
+        $adminStore = $this->getMockBuilder(StoreInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $adminStore->expects($this->atLeastOnce())
+            ->method('getId')
+            ->willReturn(0);
+
+        $nzStore = $this->getMockBuilder(StoreInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $nzStore->expects($this->atLeastOnce())
+            ->method('getId')
+            ->willReturn(1);
+
+        $auStore = $this->getMockBuilder(StoreInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $auStore->expects($this->atLeastOnce())
+            ->method('getId')
+            ->willReturn(2);
 
         $this->storeRepositoryMock->expects($this->atLeastOnce())
             ->method('getList')
-            ->willReturn(['nz' => $this->storeInterfaceMock, 'au' => $this->storeInterfaceMock]);
+            ->willReturn(['admin' => $adminStore, 'nz' => $nzStore, 'au' => $auStore]);
 
-        $this->storeInterfaceMock->expects($this->atLeastOnce())
-            ->method('getId')
-            ->willReturnOnConsecutiveCalls(1, 1, 2, 2);
-
-        $this->assertEquals($storeIds, $this->model->filterStoresByStoreIds($storeIds));
+        $this->assertSame($expectedResult, $this->model->filterStoresByStoreIds($storeIds));
     }
 }
